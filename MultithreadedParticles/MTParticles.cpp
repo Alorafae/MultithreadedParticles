5d9831b62b0163cc843e0cf1c64f4cd3cf32d9e7
Alora Boone <39552213+Alorafae@users.noreply.github.com>
2019-06-21 15:44:14 -0700

    Extremely rudimentary threading.

--- MultithreadedParticles/MTParticles.cpp
+++ MultithreadedParticles/MTParticles.cpp
@@ -3,16 +3,58 @@
 
 MTParticleSystem::MTParticleSystem()
 {
+  parts = new Particle[MAX_PARTICLES];
 }
 
 MTParticleSystem::~MTParticleSystem()
 {
+  delete parts;
 }
 
 void MTParticleSystem::Simulate(float dt)
 {
+  if (MAX_THREADS)
+  {
+    /*
+    for (unsigned i = 0; i < MAX_THREADS; ++i)
+    {
+      std::thread t1(&MTParticleSystem::MTSimulate, this, dt, i);
+      threads.push_back(t1);
+    }
+
+    for (unsigned i = 0; i < MAX_THREADS; ++i)
+    {
+      threads[i].join();
+    }
+    */
+    std::thread t1(&MTParticleSystem::MTSimulate, this, dt, 1);
+    std::thread t2(&MTParticleSystem::MTSimulate, this, dt, 2);
+    std::thread t3(&MTParticleSystem::MTSimulate, this, dt, 3);
+    std::thread t4(&MTParticleSystem::MTSimulate, this, dt, 4);
+
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+  }
+  else
+  {
+    for (unsigned i = 0; i < MAX_PARTICLES; ++i)
+    {
+      parts[i].pos += parts[i].dir * dt;
+    }
+  }
+}
+
+void MTParticleSystem::Update(float dt)
+{
+
+}
+
+void MTParticleSystem::MTSimulate(float dt, unsigned id)
+{
   for (unsigned i = 0; i < MAX_PARTICLES; ++i)
   {
-    parts[i].pos += parts[i].dir * dt;
+    printf("%d -> thread count: %d\n", id, i);
   }
 }
--- MultithreadedParticles/MTParticles.h
+++ MultithreadedParticles/MTParticles.h
@@ -4,7 +4,7 @@
 #include <vector>
 #include "glm/common.hpp"
 
-#define MAX_THREADS 4 // set to 1 (or maybe 0) for single threaded
+#define MAX_THREADS 4 // set to 0 for single threaded
 #define MAX_PARTICLES 1024
 
 struct Particle
@@ -20,12 +20,14 @@ public:
   ~MTParticleSystem();
 
   virtual void Simulate(float dt);
+  virtual void Update(float dt);
+  virtual void MTSimulate(float dt, unsigned id);
 private:
   glm::vec3 pos;
   glm::vec3 dir;
 
-  Particle parts[MAX_PARTICLES]; // make this dynamic
-  std::vector<std::thread> threads;
+  Particle *parts;
+  std::vector<std::thread*> threads;
 
   unsigned nThreads = MAX_THREADS;
 };
